5e17ebfdd379ce122f7353b9695298e6654f300c
Fernanda Vieira <fernanda.silva_sj@hotmail.com>
2020-03-13 17:18:28 -0300

    enviando dados do login utilizando vuex, e redirecionando para outra página

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,56 +1,96 @@
 <template>
-  <div class="card">
-    <header class="card-header">
-      <p class="control is-expanded card-header-title has-text-centered">Login</p>
-    </header>
+    <div class="card">
+        <header class="card-header">
+            <p class="control is-expanded card-header-title has-text-centered">Login</p>
+        </header>
 
-    <!-- CARD INPUTS -->
-    <div class="card-content">
-      <div class="content">
-        <div class="field">
-          <p class="control has-icons-left has-icons-right">
-            <input class="input" type="email" placeholder="Email" />
-            <span class="icon is-small is-left">
-              <i class="fas fa-envelope"></i>
-            </span>
-          </p>
+        <!-- CARD INPUTS -->
+        <div class="card-content">
+            <div class="content">
+                <form class="login-right" @submit.prevent>
+                    <div class="field">
+                        <p class="control has-icons-left has-icons-right">
+                            <input
+                                class="input"
+                                type="email"
+                                placeholder="Email"
+                                v-model="userLogin.email"
+                            />
+                            <span class="icon is-small is-left">
+                                <i class="fas fa-envelope"></i>
+                            </span>
+                        </p>
+                    </div>
+                    <div class="field">
+                        <p class="control has-icons-left">
+                            <input
+                                class="input"
+                                type="password"
+                                placeholder="Password"
+                                v-model="userLogin.password"
+                            />
+                            <span class="icon is-small is-left">
+                                <i class="fas fa-lock"></i>
+                            </span>
+                        </p>
+                    </div>
+                    <div class="field">
+                        <div class="control">
+                            <label class="radio">
+                                <input
+                                    v-model="userLogin.userType"
+                                    :value="1"
+                                    type="radio"
+                                    name="userType"
+                                />
+                                Professor(a)
+                            </label>
+                            <label class="radio">
+                                <input
+                                    v-model="userLogin.userType"
+                                    :value="2"
+                                    type="radio"
+                                    name="userType"
+                                />
+                                Aluno(a)
+                            </label>
+                        </div>
+                    </div>
+                    <button class="button is-black" @click="loginData">Login</button>
+                    <a>
+                        <p style="margin-top:5px">Cadastre-se aqui!</p>
+                    </a>
+                </form>
+            </div>
         </div>
-        <div class="field">
-          <p class="control has-icons-left">
-            <input class="input" type="password" placeholder="Password" />
-            <span class="icon is-small is-left">
-              <i class="fas fa-lock"></i>
-            </span>
-          </p>
-        </div>
-        <div class="field">
-          <div class="control">
-            <label class="radio">
-              <input v-model="login" :value="1" type="radio" name="user" />
-              Professor(a)
-            </label>
-            <label class="radio">
-              <input v-model="login" :value="2" type="radio" name="user" />
-              Aluno(a)
-            </label>
-          </div>
-        </div>
-        <button class="button is-black">Login</button>
-        <a>
-          <p style="margin-top:5px">Cadastre-se aqui!</p>
-        </a>
-      </div>
+        <!-- CARD INPUTS end -->
     </div>
-    <!-- CARD INPUTS end -->
-  </div>
 </template>
 <script>
 export default {
-  name: "login",
-  data (){
-    return {
-      login: null
-    };
-  }
+    name: "login",
+    data() {
+        return {
+            userLogin: {
+                email: "",
+                password: "",
+                userType: ""
+            }
+        };
+    },
+    methods: {
+        loginData() {
+            console.log(this.userLogin);
+            this.$store.dispatch("loginDataAction", this.userLogin);
+            this.$router.push({ path: "/login-data" });
+
+            // if (this.userLogin.userType == 1) {
+            //     this.$router.push({ path: "/testesP" });
+            // } else if (this.userLogin.userType == 2) {
+            //     console.log("é um aluno(a)");
+            //     this.$router.push({ path: "/testes" });
+            // }
+        }
+    }
 };
 </script>
---
+++ src/components/loginData.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Dados do login</h1>
+        <h4>Email: {{getLoginData.email}}</h4>
+        <h4>Password: *********</h4>
+        <h4>Tipo de usuário: {{getLoginData.userType}}</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "teste",
+    computed: {
+        getLoginData() {
+            return this.$store.getters.getLoginData;
+        }
+    }
+};
+</script>
+<style>
+</style>

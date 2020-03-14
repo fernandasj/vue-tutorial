54a8d18985be132902b4c10cfd620407f8a5094a
Fernanda Vieira <fernanda.silva_sj@hotmail.com>
2020-03-14 10:54:38 -0300

    refatorando rotas :boom:

---
+++ src/routes/index.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+import Login from "../components/login/Login";
+import QuestaoViewCard from "../components/questoes/QuestaoViewCard";
+import TesteEstudanteCardView from "../components/testes/TesteEstudanteCardView";
+import TesteProfessorCardView from "../components/testesProff/TesteProfessorCardView";
+import RespostaAlgoritmo from "../components/respostas/RespostaAlgoritmo";
+import RespostaSubjetiva from "../components/respostas/RespostaSubjetiva";
+import RespostaObjetiva from "../components/respostas/RespostaObjetiva";
+import Resultado from "../components/testes/Resultado";
+import LoginData from "../components/LoginData";
+
+export const router = new VueRouter({
+  mode: "history",
+  routes: [
+    { path: "/", component: Login },
+    { path: "/questions", component: QuestaoViewCard },
+    { path: "/tests", component: TesteEstudanteCardView },
+    { path: "/testesP", component: TesteProfessorCardView },
+    { path: "/resposta-algoritmo", component: RespostaAlgoritmo },
+    { path: "/resposta-subjetiva", component: RespostaSubjetiva },
+    { path: "/resposta-objetiva", component: RespostaObjetiva },
+    { path: "/resultado", component: Resultado },
+    { path: "/login-data", component: LoginData }
+  ]
+});

5748f5c45e49227e98b9bf6b46da39271d7f2207
Fernanda Vieira <fernanda.silva_sj@hotmail.com>
2020-03-24 23:07:49 -0300

    configurando VueFormWizard

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import axios from "axios";
 import VueRouter from "vue-router";
+import VueFormWizard from "vue-form-wizard";
+import "vue-form-wizard/dist/vue-form-wizard.min.css";
 
 import { store } from "./store";
 import { router } from "./routes";
@@ -11,6 +13,7 @@ import Vuex from "vuex";
 Vue.prototype.$axios = axios;
 Vue.prototype.$store = store;
 
+Vue.use(VueFormWizard);
 Vue.use(VueRouter);
 Vue.use(Vuex);
 
